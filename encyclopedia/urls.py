e7c43af5f3976786908b33116672374b77954cbe
kspr9 <kaspar.pae@gmail.com>
2022-07-14 09:54:56 +0300

    cs50web project 1 entry page and index page is done

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<slug:entry_name>", views.entryview, name="entry"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,9 +2,43 @@ from django.shortcuts import render
 
 from . import util
 
+#import markdown as md
+import markdown2 as md2
+from markdown.extensions import Extension
+from bs4 import BeautifulSoup as bs
+
 
 def index(request):
+
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
     })
 
+def get_entry_html(entry_name):
+    # accessing the md file contents
+    entry = util.get_entry(entry_name)
+    # generating html from md file
+    html = md2.markdown(entry)
+
+    return html
+
+def get_entry_title(entry_name):
+    # for separating title from all html
+    soup = bs(get_entry_html(entry_name), 'html.parser')
+    title = soup.find("h1").string
+    
+    return title
+
+def entryview(request, entry_name):
+    if not util.get_entry(entry_name):
+        return render(request, "encyclopedia/notfound.html",)
+    
+    # entry = util.get_entry(entry_name)
+    html = get_entry_html(entry_name)
+    title = get_entry_title(entry_name)
+
+    return render(request, "encyclopedia/entry.html", {
+            # "entry": entry,
+            "entry_html": html,
+            "title": title,
+    })
